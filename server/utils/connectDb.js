5affac66bdb3ad027c2b6543a7e5f7f01d21fcf8
Anshul Srivastava <anshulakgec@gmail.com>
2026-02-24 01:43:10 +0530

    commit

--- server/utils/connectDb.js
+++ server/utils/connectDb.js
@@ -3,12 +3,11 @@ import mongoose from "mongoose";
 const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            serverSelectionTimeoutMS: 5000,
-            socketTimeoutMS: 45000,
+            
         });
         console.log("Database connected successfully");
     } catch (error) {
-        console.error("Error connecting to database:", error.message);
+        console.error(error);
         process.exit(1);
     }
 }
