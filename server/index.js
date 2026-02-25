d5e6113e73dff636c3daa39887469754acfb608c
Anshul Srivastava <anshulakgec@gmail.com>
2026-02-25 20:36:35 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 import userRouter from "./routes/user.route.js";
 import notesRouter from "./routes/generate.route.js";
+import pdfRouter from "./routes/pdf.route.js";
 dotenv.config();
 
 
@@ -27,6 +28,7 @@ app.get("/",(req,res)=>{
 app.use("/api/auth",authRouter);
 app.use("/api/user",userRouter);
 app.use("/api/notes",notesRouter);
+app.use("/api/pdf",pdfRouter);
 
 app.listen(PORT,async ()=>{
     console.log(`Server running on port ${process.env.PORT}`);
